aba6d37296df12a067d00e901aa4c6cfcd1d29e3
duck_alan <duckalan2@gmail.com>
2023-10-16 23:16:39 +0300

    Minor

--- Lab19-20/Register.h
+++ Lab19-20/Register.h
@@ -1,10 +1,25 @@
 #pragma once
 
+#include <array>
 #include "Memory.h"
 
 class Register
 {
+public:
+	static const int MemoryWidth = 10;
+
 private:
-	Memory memory;
+	bool resetInput_;
+
+	bool setInput_;
+
+	std::array<Memory, MemoryWidth> memory_;
+
+	std::array<int, Memory::InputCount * MemoryWidth> inputValues_;
+
+public:
+	Register(std::string name);
+
+	void SetInputValues(std::array<int, Memory::InputCount* MemoryWidth> inputValues);
 };
 
